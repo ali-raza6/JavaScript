9352953a4804f06d86301a31367499902ce7a12b
ali-raza6 <ali.gcuf@gmail.com>
2025-01-01 08:17:03 +0000

    Mutator Methods Part-1

---
+++ 001.js
@@ -0,0 +1,21 @@
+
+//Arrays
+//Mutator Methods >>> These methods modify the array itself.
+let cities=["London","New York","Toronto"]; // An array declared with three string items.
+console.log(cities);
+
+cities.push("Karachi","Lahore","Quetta","Peshawar")//push() method is used to add 1 or multiple elements in the last of an array.
+console.log(cities);
+
+cities.pop();// pop() method is used to delete a single item from the array, it will delete only single and last item of an array.
+console.log(cities);
+
+cities.unshift("Tokyo","Bejing","New Dehli","Tehran"); // unshifit() method is used to add 1 or multiple items at the strat of an array.
+console.log(cities);
+
+cities.shift(); // shift() method is used to remove only 1st item from the array, only 1 and only 1st.
+console.log(cities);
+
+
+
+ 
